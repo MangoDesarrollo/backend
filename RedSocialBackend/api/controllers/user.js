c8b21601b4f5ea7c17d037f70d2cb7e963f8540f
Desarrollo <54876209+MangoDesarrollo@users.noreply.github.com>
2020-12-03 14:57:05 -0600

    UpdateUsers

--- RedSocialBackend/api/controllers/user.js
+++ RedSocialBackend/api/controllers/user.js
@@ -4,6 +4,11 @@ let bcrypt = require('bcrypt-node');
 const {param} = require('../app');
 let User = require('../models/user');
 let jwt = require('../services/jwt');
+let mongoosePaginate = require('mongoose-pagination');
+let path = require('path');
+let fs = require('fs');
+var fileupload = require("express-fileupload"); 
+
 
 function home(req, res){
     res.status(200).send(
@@ -111,14 +116,160 @@ function loginUsers(req, res){
             message: "No se encontró el e-mail del usuario o la contraseña"
         })
     }
+}
 
+function getUser(req, res){
+    let userId = req.params.id;
 
+    User.findById(userId, (err, user) =>{
+        if(err){
+            return res.status(500).send({
+                message: 'Hubo error en la petición'
+            })
+        }
 
+        if(!user){
+            return res.status(200).send({
+                message: 'Usuario no encontrado'
+            })
+        }
+
+        return res.status(200).send({user})
+
+    })
+}
+
+function getUsers(req, res){
+    let page = 1;
+
+    if(req.params.page){
+        page = req.params.page
+    }
+
+    let docsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, docsPerPage, (err, users, total) => {
+        if(err){
+            return res.status(500).send({
+                message: 'Hubo un error consultando los usuarios'
+            })
+        }
+
+        if(!users) return res.status(200).send({
+            message: "No hay usuarios para mostrar"
+        })
+
+        return res.status(200).send({
+            users,
+            total,
+            pages: Math.ceil(total/docsPerPage)
+        })
+    })
+}
+
+// Modificación de los datos del usuario
+
+function updateUser(req, res){
+
+    if(!req.params.id){
+        return res.status(500).send({
+            message: "No se envió el id del usuario"
+        })
+    }
+
+    let userId = req.params.id;
+    let userUpdate = req.body;
+
+    delete userUpdate.password;
+
+    if(userId != req.user.sub){
+        return res.status(500).send({
+            message: "El usuario no tiene permisos para modificar este usuario"
+        })
+    }
+
+    User.findByIdAndUpdate(userId, userUpdate, {new:true}, (err, userUpdate)=> {
+        if(err){
+            return res.status(500).send({
+                message: 'Error en actualización'
+            })
+        }
+
+        if(!userUpdate){
+            return res.status(500).send({
+                message: 'No fueron enviados datos para hacer la actualiación'
+            })
+        }
+
+        return res.status(200).send({
+            userUpdate
+        })
+
+    })
+}
+
+// Cargar la foto del usuario
+
+function updateImage(req, res){
+    let userId = req.params.id;
+    
+    if(userId != req.user.sub){
+        return res.status(500).send({
+            message: "El usuario no tiene permisos para modificar este usuario"
+        })
+    }
+
+    if(req.files){
+        console.log("Hasta aqui va la solicitud");
+        let file_path = req.files.image.path;
+        let file_split = file_path.split('\\');
+        console.log(file_split);
+
+        let file_name = file_split[2];
+        let ext_file = file_name.split('\.')[1];
+        console.log(ext_file);
+        
+        if(ext_file == "png" | ext_file == "jpg" | ext_file == "gif" | ext_file == "svg"){
+            User.findByIdAndUpdate(userId, {image: file_name}, {new:true}, (err, userUpdated) => {
+                if(err){
+                    return res.status(500).send({
+                        message: "Hubo un error al actualizar la imagen"
+                    })
+                }
+
+                if(!userUpdated){
+                    return res.status(200).send({
+                        message: "No se encontró un usuario para actualizar"
+                    })
+                }
+
+                return res.status(200).send({
+                    message: "La imagen se actualizó correctamente",
+                    userUpdated
+                })
+
+            })
+        }else{            
+            fs.unlink(file_path, () => {
+                return res.status(200).send({
+                    message: "La extension del archivo no es correcta"
+                })
+            })
+        }
+    }else{
+        return res.status(200).send({
+            message: "No se ha cargado ningun archivo"
+        })
+    }
 }
 
 module.exports = {
     home,
     pruebas,
     saveUser,
-    loginUsers
+    loginUsers,
+    getUser,
+    getUsers,
+    updateUser,
+    updateImage
 }
--- RedSocialBackend/api/routes/user.js
+++ RedSocialBackend/api/routes/user.js
@@ -1,9 +1,14 @@
 'use strict'
 
 let express = require('express');
+let multipart = require('connect-multiparty')
 let UserController = require('../controllers/user');
 const { model } = require('../models/user');
 let mid_auth = require('../middlewares/auth');
+
+let md_upload = multipart({
+    uploadDir: './uploads/users'
+})
 
 let api = express.Router() // Aquí están todos los métodos con los que nosotros podemos hacer cualquier tipo de peticiones get/post/patch...
 
@@ -11,5 +16,11 @@ api.get('/home', UserController.home);
 api.get('/pruebas', mid_auth.validateAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUsers);
+// Traer información de los usuarios
+api.get('/user/:id', mid_auth.validateAuth, UserController.getUser);
+api.get('/users/:page?', mid_auth.validateAuth, UserController.getUsers);
+// Modificación de los datos del usuario
+api.put('/update-user/:day', mid_auth.validateAuth, UserController.updateUser);
+api.post('/upload-image/:id', [mid_auth.validateAuth, md_upload], UserController.updateImage);
 
 module.exports = api;
